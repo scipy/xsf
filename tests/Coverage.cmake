71f30840af1ecdf7dc3d82166ccac046823e353a
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-13 17:29:11 +0800

    TST: Generating coverage report

---
+++ tests/Coverage.cmake
@@ -0,0 +1,30 @@
+if(CMAKE_BUILD_TYPE STREQUAL "Coverage")
+
+# Enable coverage compilation option
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
+endif()
+if(MSVC)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /coverage")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /coverage")
+endif()
+
+# Add custom targets for generating coverage reports
+add_custom_target(coverage
+    COMMAND /usr/bin/lcov --capture --directory . --output-file coverage.info
+    COMMAND /usr/bin/lcov --output-file coverage.info --extract coverage.info '*/include/xsf/*'
+    COMMAND /usr/bin/lcov --list coverage.info
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMENT "Generating coverage report"
+)
+
+# Generate coverage reports in HTML format
+add_custom_target(coverage_html
+    COMMAND genhtml --demangle-cpp --legend coverage.info --output-directory coverage_report
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMENT "Generating HTML coverage report"
+)
+add_dependencies(coverage_html coverage)
+
+endif() # CMAKE_BUILD_TYPE=Coverage
