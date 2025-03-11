9476b859c530a6d7ff71f7180d6790be324df35a
steppi <1953382+steppi@users.noreply.github.com>
2025-03-11 12:51:14 -0400

    Add real hyp2f1 tests

--- tests/test_hyp2f1.cpp
+++ tests/test_hyp2f1.cpp
@@ -28,6 +28,7 @@ TEST_CASE("hyp2f1 complex scipy.special cases", "[hyp2f1][complex][scipy-special
     auto [desired, fallback] = output;
     auto out = xsf::hyp2f1(a, b, c, z);
     auto error = xsf::extended_relative_error(out, desired);
+    tol = adjust_tolerance(tol);
     INFO("a := " << std::setprecision(std::numeric_limits<double>::max_digits10) << a << '\n'
 	 << "b := " << b << '\n'
 	 << "c := " << c << '\n'
@@ -35,7 +36,32 @@ TEST_CASE("hyp2f1 complex scipy.special cases", "[hyp2f1][complex][scipy-special
 	 << "out := " << out << '\n'
 	 << "desired := " << desired << '\n'
 	 << "error := " << error << '\n'
-	 << "tolerance := " << 2*tol << '\n'
+	 << "tolerance := " << tol << '\n'
 	);
-    REQUIRE(error < 2*tol);
+    REQUIRE(error <= tol);
+}
+
+TEST_CASE("hyp2f1 real scipy.special cases", "[hyp2f1][real][scipy-special]") {
+    auto [input, output, tol] = GENERATE(
+        xsf_test_cases<
+            std::tuple<double, double, double, double>, std::tuple<double, bool>, double>(
+            hyp2f1_tables_path / "In_d_d_d_d-d.parquet", hyp2f1_tables_path / "Out_d_d_d_d-d.parquet",
+            hyp2f1_tables_path / "Err_d_d_d_d-d_gcc-linux-x86_64.parquet"
+        )
+    );
+    auto [a, b, c, z] = input;
+    auto [desired, fallback] = output;
+    auto out = xsf::hyp2f1(a, b, c, z);
+    auto error = xsf::extended_relative_error(out, desired);
+    tol = adjust_tolerance(tol);
+    INFO("a := " << std::setprecision(std::numeric_limits<double>::max_digits10) << a << '\n'
+	 << "b := " << b << '\n'
+	 << "c := " << c << '\n'
+	 << "z := " << z << '\n'
+	 << "out := " << out << '\n'
+	 << "desired := " << desired << '\n'
+	 << "error := " << error << '\n'
+	 << "tolerance := " << tol << '\n'
+	);
+    REQUIRE(error <= tol);
 }
