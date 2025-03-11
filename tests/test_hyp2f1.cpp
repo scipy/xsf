06cb79e5bf6f8bc5bf3383bd8e250243dd3d4df1
steppi <1953382+steppi@users.noreply.github.com>
2025-03-11 12:23:53 -0400

    Update INFO string to match what CAPTURE would do if it worked well

--- tests/test_hyp2f1.cpp
+++ tests/test_hyp2f1.cpp
@@ -26,19 +26,16 @@ TEST_CASE("hyp2f1 complex scipy.special cases", "[hyp2f1][complex][scipy-special
     );
     auto [a, b, c, z] = input;
     auto [desired, fallback] = output;
-    auto actual = xsf::hyp2f1(a, b, c, z);
-    auto error = xsf::extended_relative_error(actual, desired);
-    INFO(
-        "Inputs: " << std::setprecision(std::numeric_limits<double>::max_digits10) << '\n'
-                   << "a: " << a << '\n'
-                   << "b: " << b << '\n'
-                   << "c: " << c << '\n'
-                   << "z: " << z << '\n'
-                   << "Output: \n"
-                   << "desired: " << desired << '\n'
-                   << "actual: " << actual << '\n'
-                   << "error: " << error << '\n'
-                   << "tolerance: " << tol << '\n'
-    );
-    REQUIRE(error <= 2 * tol);
+    auto out = xsf::hyp2f1(a, b, c, z);
+    auto error = xsf::extended_relative_error(out, desired);
+    INFO("a := " << std::setprecision(std::numeric_limits<double>::max_digits10) << a << '\n'
+	 << "b := " << b << '\n'
+	 << "c := " << c << '\n'
+	 << "z := " << z << '\n'
+	 << "out := " << out << '\n'
+	 << "desired := " << desired << '\n'
+	 << "error := " << error << '\n'
+	 << "tolerance := " << 2*tol << '\n'
+	);
+    REQUIRE(error < 2*tol);
 }
