3acbaf687d47e0af7b828033d3d7535e58185e52
steppi <1953382+steppi@users.noreply.github.com>
2025-04-05 23:41:26 -0400

    Remove tests for deprecated bdtr* overloads that aren't even in xsf

--- tests/scipy_special_tests/test_bdtri.cpp
+++ tests/scipy_special_tests/test_bdtri.cpp
@@ -22,20 +22,3 @@ TEST_CASE("bdtri dpd->d scipy_special_tests", "[bdtri][dpd->d][scipy_special_tes
     CAPTURE(k, n, y, out, desired, error, tol, fallback);
     REQUIRE(error <= tol);
 }
-
-TEST_CASE("bdtri ddd->d scipy_special_tests", "[bdtri][ddd->d][scipy_special_tests]") {
-    SET_FP_FORMAT()
-    auto [input, output, tol] =
-        GENERATE(xsf_test_cases<std::tuple<double, double, double>, std::tuple<double, bool>, double>(
-            tables_path / "In_d_d_d-d.parquet", tables_path / "Out_d_d_d-d.parquet",
-            tables_path / ("Err_d_d_d-d_" + get_platform_str() + ".parquet")
-        ));
-
-    auto [k, n, y] = input;
-    auto [desired, fallback] = output;
-    auto out = xsf::bdtri(k, n, y);
-    auto error = xsf::extended_relative_error(out, desired);
-    tol = adjust_tolerance(tol);
-    CAPTURE(k, n, y, out, desired, error, tol, fallback);
-    REQUIRE(error <= tol);
-}
