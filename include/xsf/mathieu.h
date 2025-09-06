0adc5a6f863793695f07c9e530629702b35dd82d
Stuart Brorson <brorson@theworld.com>
2025-09-05 20:51:24 -0400

    Update mathieu.h with improved docstrings and comments.

--- include/xsf/mathieu.h
+++ include/xsf/mathieu.h
@@ -1,228 +1,357 @@
 #pragma once
 
 #include "error.h"
-#include "specfun/specfun.h"
+#include "mathieu/matrix_utils.h"
+#include "mathieu/make_matrix.h"
+#include "mathieu/mathieu_coeffs.h"
+#include "mathieu/mathieu_eigs.h"
+#include "mathieu/besseljyd.h"
+#include "mathieu/mathieu_fcns.h"
+
+/*
+ *
+ * This is part of the Mathieu function suite -- a reimplementation
+ * of the Mathieu functions for Scipy.  This file #includes all the
+ * fcn impls in the mathieu/ subdirectory, and provides translation
+ * from the Scipy call to the calling signature I implemented in my
+ * reimplementation.
+ *
+ * Stuart Brorson, Summer 2025.
+ * 
+ */
 
 namespace xsf {
 
-template <typename T>
-T sem_cva(T m, T q);
-
-template <typename T>
-void sem(T m, T q, T x, T &csf, T &csd);
-
-/* Mathieu functions */
 /* Characteristic values */
+//-------------------------------------------------------------
+/**
+ * Mathieu characteristic values (eigenvalues) for even parity functions.
+ *
+ * Even parity characteristic values a.
+ *
+ * @param  m    Eigenvalue order.  Must be positive integer less than 500.
+ * @param  q    Mathieu parameter q.  Real number.
+ * @return      Mathieu eigenvalue a.
+ */
 template <typename T>
 T cem_cva(T m, T q) {
-    int int_m, kd = 1;
+  // This returns the even Mathieu characteristic value (eigenvalue) a.
 
-    if ((m < 0) || (m != floor(m))) {
-        set_error("mathieu_a", SF_ERROR_DOMAIN, NULL);
-        return std::numeric_limits<T>::quiet_NaN();
-    }
-    int_m = (int)m;
-    if (q < 0) {
-        /* https://dlmf.nist.gov/28.2#E26 */
-        if (int_m % 2 == 0) {
-            return cem_cva(m, -q);
-        } else {
-            return sem_cva(m, -q);
-        }
-    }
+  // Check for invalid Mathieu order.
+  if ((m < 0) || (m != floor(m))) {
+    set_error("mathieu_a", SF_ERROR_DOMAIN, NULL);
+    return std::numeric_limits<T>::quiet_NaN();
+  }
+  
+  // Must cast to correct types prior to fcn call.
+  int im = static_cast<int>(m);
+  double dq = static_cast<double>(q);
+  double da;
+    
+  int retcode = xsf::mathieu::mathieu_a(im, dq, &da);
+  if (retcode != SF_ERROR_OK) {
+    set_error("mathieu_a", SF_ERROR_NO_RESULT, NULL);
+    return std::numeric_limits<T>::quiet_NaN();
+  }
 
-    if (int_m % 2) {
-        kd = 2;
-    }
-    return specfun::cva2(kd, int_m, q);
+  // Now cast back.
+  T a = static_cast<T>(da);
+  return a;
 }
 
+//-------------------------------------------------------------  
 template <typename T>
+/**
+ * Mathieu characteristic values (eigenvalues) b for odd functions.
+ *
+ *
+ * Odd parity characteristic values b.
+ *
+ * @param  m    Eigenvalue order.  Must be positive integer less than 500.
+ * @param  q    Mathieu parameter q.  Real number.
+ * @return      Mathieu eigenvalue b.
+ */
 T sem_cva(T m, T q) {
-    int int_m, kd = 4;
+  // This returns the odd Mathieu characteristic value (eigenvalue) b.
 
-    if ((m <= 0) || (m != floor(m))) {
-        set_error("mathieu_b", SF_ERROR_DOMAIN, NULL);
-        return std::numeric_limits<T>::quiet_NaN();
-    }
-    int_m = (int)m;
-    if (q < 0) {
-        /* https://dlmf.nist.gov/28.2#E26 */
-        if (int_m % 2 == 0) {
-            return sem_cva(m, -q);
-        } else {
-            return cem_cva(m, -q);
-        }
-    }
-    if (int_m % 2) {
-        kd = 3;
-    }
-    return specfun::cva2(kd, int_m, q);
+  // Check for invalid Mathieu order.
+  if ((m < 1) || (m != floor(m))) {
+    set_error("mathieu_b", SF_ERROR_DOMAIN, NULL);
+    return std::numeric_limits<T>::quiet_NaN();
+  }
+
+  // Must cast to correct types prior to fcn call.
+  int im = static_cast<int>(m);
+  double dq = static_cast<double>(q);
+  double db;
+    
+  int retcode = xsf::mathieu::mathieu_b(im, dq, &db);
+  if (retcode != SF_ERROR_OK) {
+    set_error("mathieu_b", SF_ERROR_NO_RESULT, NULL);
+    return std::numeric_limits<T>::quiet_NaN();
+  }
+
+  // Now cast back.
+  T b = static_cast<T>(db);
+  return b;
 }
 
+
+//---------------------------------------------------------------
 /* Mathieu functions */
+/**
+ * Even parity Mathieu angular function ce(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Real number.
+ * @param  x    Angular coordinate x (radians).  Real number.
+ * @param  csf  Value of function.  Real number.
+ * @param  csd  Value of derivative w.r.t. x.  Real number.
+ */
 template <typename T>
 void cem(T m, T q, T x, T &csf, T &csd) {
-    int int_m, kf = 1, sgn;
-    T f = 0.0, d = 0.0;
-    if ((m < 0) || (m != floor(m))) {
-        csf = std::numeric_limits<T>::quiet_NaN();
-        csd = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_cem", SF_ERROR_DOMAIN, NULL);
+
+  if ((m < 0) || (m != floor(m))) {
+    csf = std::numeric_limits<T>::quiet_NaN();
+    csd = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_ce", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double dcsf;
+    double dcsd;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_ce(im, dq, dx, &dcsf, &dcsd);
+    if (retcode != SF_ERROR_OK) {
+      csf = std::numeric_limits<T>::quiet_NaN();
+      csd = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_ce", (sf_error_t) retcode, NULL);
     } else {
-        int_m = (int)m;
-        if (q < 0) {
-            /* https://dlmf.nist.gov/28.2#E34 */
-            if (int_m % 2 == 0) {
-                sgn = ((int_m / 2) % 2 == 0) ? 1 : -1;
-                cem(m, -q, 90 - x, f, d);
-                csf = sgn * f;
-                csd = -sgn * d;
-
-            } else {
-                sgn = ((int_m / 2) % 2 == 0) ? 1 : -1;
-                sem(m, -q, 90 - x, f, d);
-                csf = sgn * f;
-                csd = -sgn * d;
-            }
-        } else {
-            using specfun::Status;
-            Status status = specfun::mtu0(kf, int_m, q, x, &csf, &csd);
-            if (status != Status::OK) {
-                csf = std::numeric_limits<T>::quiet_NaN();
-                csd = std::numeric_limits<T>::quiet_NaN();
-                sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-                set_error("mathieu_cem", sf_error, NULL);
-            }
-        }
+      csf = static_cast<T>(dcsf);
+      csd = static_cast<T>(dcsd);
     }
+  }
 }
+  
 
+//---------------------------------------------------------------  
+/**
+ * Odd parity Mathieu angular function se(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Real number.
+ * @param  x    Angular coordinate x (radians).  Real number.
+ * @param  ssf  Value of function.  Real number.
+ * @param  ssd  Value of derivative w.r.t. x.  Real number
+ */
 template <typename T>
-void sem(T m, T q, T x, T &csf, T &csd) {
-    int int_m, kf = 2, sgn;
-    T f = 0.0, d = 0.0;
-    if ((m < 0) || (m != floor(m))) {
-        csf = std::numeric_limits<T>::quiet_NaN();
-        csd = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_sem", SF_ERROR_DOMAIN, NULL);
+void sem(T m, T q, T x, T &ssf, T &ssd) {
+
+  if ((m < 1) || (m != floor(m))) {
+    ssf = std::numeric_limits<T>::quiet_NaN();
+    ssd = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_sem", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double dssf;
+    double dssd;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_se(im, dq, dx, &dssf, &dssd);
+    if (retcode != SF_ERROR_OK) {
+      ssf = std::numeric_limits<T>::quiet_NaN();
+      ssd = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_sem", (sf_error_t) retcode, NULL);
     } else {
-        int_m = (int)m;
-        if (int_m == 0) {
-            csf = 0;
-            csd = 0;
-        } else if (q < 0) {
-            /* https://dlmf.nist.gov/28.2#E34 */
-            if (int_m % 2 == 0) {
-                sgn = ((int_m / 2) % 2 == 0) ? -1 : 1;
-                sem(m, -q, 90 - x, f, d);
-                csf = sgn * f;
-                csd = -sgn * d;
-            } else {
-                sgn = ((int_m / 2) % 2 == 0) ? 1 : -1;
-                cem(m, -q, 90 - x, f, d);
-                csf = sgn * f;
-                csd = -sgn * d;
-            }
-        } else {
-            using specfun::Status;
-            Status status = specfun::mtu0(kf, int_m, q, x, &csf, &csd);
-            if (status != Status::OK) {
-                csf = std::numeric_limits<T>::quiet_NaN();
-                csd = std::numeric_limits<T>::quiet_NaN();
-                sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-                set_error("mathieu_sem", sf_error, NULL);
-            }
-        }
+      ssf = static_cast<T>(dssf);
+      ssd = static_cast<T>(dssd);
     }
+  }
 }
 
+//---------------------------------------------------------------  
+/**
+ * Even parity modified (radial) Mathieu function of first kind Mc1(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Positive real number
+ * @param  x    Radial coordinate x.  Positive real number.
+ * @param  f1r  Value of function.  Real number.
+ * @param  d1r  Value of derivative w.r.t. x.  Real number
+ */
 template <typename T>
 void mcm1(T m, T q, T x, T &f1r, T &d1r) {
-    int int_m, kf = 1, kc = 1;
-    T f2r = 0.0, d2r = 0.0;
 
-    if ((m < 0) || (m != floor(m)) || (q < 0)) {
-        f1r = std::numeric_limits<T>::quiet_NaN();
-        d1r = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_modcem1", SF_ERROR_DOMAIN, NULL);
+  if ((m < 0) || (m != floor(m))) {
+    f1r = std::numeric_limits<T>::quiet_NaN();
+    d1r = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_mcm1", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double df1r;
+    double dd1r;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_modmc1(im, dq, dx, &df1r, &dd1r);
+    if (retcode != SF_ERROR_OK) {
+      f1r = std::numeric_limits<T>::quiet_NaN();
+      d1r = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_mcm1", (sf_error_t) retcode, NULL);
     } else {
-        using specfun::Status;
-        int_m = (int)m;
-        Status status = specfun::mtu12(kf, kc, int_m, q, x, &f1r, &d1r, &f2r, &d2r);
-        if (status != Status::OK) {
-            f1r = std::numeric_limits<T>::quiet_NaN();
-            d1r = std::numeric_limits<T>::quiet_NaN();
-            sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-            set_error("mathieu_modcem1", sf_error, NULL);
-        }
+      f1r = static_cast<T>(df1r);
+      d1r = static_cast<T>(dd1r);
     }
+  }
 }
 
+//---------------------------------------------------------------  
+/**
+ * Odd parity modified (radial) Mathieu function of first kind Ms1(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Positive real number
+ * @param  x    Radial coordinate x.  Positive real number.
+ * @param  f1r  Value of function.  Real number.
+ * @param  d1r  Value of derivative w.r.t. x.  Real number
+ */
 template <typename T>
 void msm1(T m, T q, T x, T &f1r, T &d1r) {
-    int int_m, kf = 2, kc = 1;
-    T f2r = 0.0, d2r = 0.0;
 
-    if ((m < 1) || (m != floor(m)) || (q < 0)) {
-        f1r = std::numeric_limits<T>::quiet_NaN();
-        d1r = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_modsem1", SF_ERROR_DOMAIN, NULL);
+  if ((m < 1) || (m != floor(m))) {
+    f1r = std::numeric_limits<T>::quiet_NaN();
+    d1r = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_msm1", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double df1r;
+    double dd1r;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_modms1(im, dq, dx, &df1r, &dd1r);
+    if (retcode != SF_ERROR_OK) {
+      f1r = std::numeric_limits<T>::quiet_NaN();
+      d1r = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_msm1", (sf_error_t) retcode, NULL);
     } else {
-        using specfun::Status;
-        int_m = (int)m;
-        Status status = specfun::mtu12(kf, kc, int_m, q, x, &f1r, &d1r, &f2r, &d2r);
-        if (status != Status::OK) {
-            f1r = std::numeric_limits<T>::quiet_NaN();
-            d1r = std::numeric_limits<T>::quiet_NaN();
-            sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-            set_error("mathieu_modsem1", sf_error, NULL);
-        }
+      f1r = static_cast<T>(df1r);
+      d1r = static_cast<T>(dd1r);
     }
+  }
+
 }
 
+//---------------------------------------------------------------  
+/**
+ * Even parity modified (radial) Mathieu function of second kind Mc2(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Positive real number
+ * @param  x    Radial coordinate x.  Positive real number.
+ * @param  f2r  Value of function.  Real number.
+ * @param  d2r  Value of derivative w.r.t. x.  Real number
+ */
 template <typename T>
 void mcm2(T m, T q, T x, T &f2r, T &d2r) {
-    int int_m, kf = 1, kc = 2;
-    T f1r = 0.0, d1r = 0.0;
 
-    if ((m < 0) || (m != floor(m)) || (q < 0)) {
-        f2r = std::numeric_limits<T>::quiet_NaN();
-        d2r = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_modcem2", SF_ERROR_DOMAIN, NULL);
+  if ((m < 0) || (m != floor(m))) {
+    f2r = std::numeric_limits<T>::quiet_NaN();
+    d2r = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_mcm2", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double df2r;
+    double dd2r;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_modmc2(im, dq, dx, &df2r, &dd2r);
+    if (retcode != SF_ERROR_OK) {
+      f2r = std::numeric_limits<T>::quiet_NaN();
+      d2r = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_mcm2", (sf_error_t) retcode, NULL);
     } else {
-        using specfun::Status;
-        int_m = (int)m;
-        Status status = specfun::mtu12(kf, kc, int_m, q, x, &f1r, &d1r, &f2r, &d2r);
-        if (status != Status::OK) {
-            f2r = std::numeric_limits<T>::quiet_NaN();
-            d2r = std::numeric_limits<T>::quiet_NaN();
-            sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-            set_error("mathieu_modcem2", sf_error, NULL);
-        }
+      f2r = static_cast<T>(df2r);
+      d2r = static_cast<T>(dd2r);
     }
+  }
+
 }
 
+//---------------------------------------------------------------  
+/**
+ * Odd parity modified (radial) Mathieu function of second kind Ms2(m, q, x)
+ *
+ * This implementation of ce follows the definitions on the
+ * DLMF, https://dlmf.nist.gov/28
+ *
+ * @param  m    Function order.  Must be positive integer less than 500.
+ * @param  q    Parameter q.  Positive real number
+ * @param  x    Radial coordinate x.  Positive real number.
+ * @param  f2r  Value of function.  Real number.
+ * @param  d2r  Value of derivative w.r.t. x.  Real number
+ */
 template <typename T>
 void msm2(T m, T q, T x, T &f2r, T &d2r) {
-    int int_m, kf = 2, kc = 2;
-    T f1r = 0.0, d1r = 0.0;
 
-    if ((m < 1) || (m != floor(m)) || (q < 0)) {
-        f2r = std::numeric_limits<T>::quiet_NaN();
-        d2r = std::numeric_limits<T>::quiet_NaN();
-        set_error("mathieu_modsem2", SF_ERROR_DOMAIN, NULL);
+  if ((m < 1) || (m != floor(m))) {
+    f2r = std::numeric_limits<T>::quiet_NaN();
+    d2r = std::numeric_limits<T>::quiet_NaN();
+    set_error("mathieu_msm2", SF_ERROR_DOMAIN, NULL);
+  } else {
+    
+    // Must cast to correct types prior to fcn call.
+    int im = static_cast<int>(m);
+    double dq = static_cast<double>(q);
+    double dx = static_cast<double>(x);
+    double df2r;
+    double dd2r;      
+    
+    // Call fcn and cast back.
+    int retcode = xsf::mathieu::mathieu_modms2(im, dq, dx, &df2r, &dd2r);
+    if (retcode != SF_ERROR_OK) {
+      f2r = std::numeric_limits<T>::quiet_NaN();
+      d2r = std::numeric_limits<T>::quiet_NaN();
+      set_error("mathieu_msm2", (sf_error_t) retcode, NULL);
     } else {
-        using specfun::Status;
-        int_m = (int)m;
-        Status status = specfun::mtu12(kf, kc, int_m, q, x, &f1r, &d1r, &f2r, &d2r);
-        if (status != Status::OK) {
-            f2r = std::numeric_limits<T>::quiet_NaN();
-            d2r = std::numeric_limits<T>::quiet_NaN();
-            sf_error_t sf_error = status == Status::NoMemory ? SF_ERROR_MEMORY : SF_ERROR_OTHER;
-            set_error("mathieu_modsem2", sf_error, NULL);
-        }
+      f2r = static_cast<T>(df2r);
+      d2r = static_cast<T>(dd2r);
     }
+  }
 }
 
+  
 } // namespace xsf
