fd4560212182caa09153fa914bc26cdec499db61
Florian Bourgey <bourgeyflorian@gmail.com>
2026-01-12 11:04:49 -0500

    Fix special case hyp2f1(-1,b,c,z) (#85)
    * fix special case hyp2f1(-1,b,c,z)
    
    * skip xsfref tests for a=-1 and c!=0

---
+++ tests/xsf_tests/test_hyp2f1.cpp
@@ -0,0 +1,39 @@
+#include "../testing_utils.h"
+#include <tuple>
+#include <xsf/hyp2f1.h>
+
+TEST_CASE("hyp2f1(-1, b, c, z) / gh-4446", "[hyp2f1][xsf_tests]") {
+    constexpr double rtol = 1e-14;
+    constexpr double atol = 1e-14; // absolute tolerance
+
+    constexpr int size = 201;
+    std::vector<double> b_values(size), c_values(size), z_values(size);
+
+    for (int i = 0; i < size; ++i) {
+        b_values[i] = -10.0 + i * 0.1;
+        c_values[i] = -10.0 + i * 0.1;
+        z_values[i] = -10.0 + i * 0.1;
+    }
+
+    for (double b : b_values) {
+        for (double c : c_values) {
+            if (c == 0.0)
+                continue;
+
+            for (double z : z_values) {
+                double expected = 1 - (b / c) * z;
+                double result = xsf::hyp2f1(-1.0, b, c, z);
+
+                double error;
+                if (std::abs(expected) < atol) {
+                    error = std::abs(result - expected);
+                } else {
+                    error = xsf::extended_relative_error(result, expected);
+                }
+
+                CAPTURE(b, c, z, result, expected, rtol, atol, error);
+                REQUIRE(error <= std::max(rtol * std::abs(expected), atol));
+            }
+        }
+    }
+}
