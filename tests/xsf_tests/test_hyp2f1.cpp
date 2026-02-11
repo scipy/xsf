70065186cf6579e110bc1ec3758d29d69e3117ce
steppi <1953382+steppi@users.noreply.github.com>
2026-02-11 12:49:23 -0500

    Add test for hyp2f1 all nan inputs

--- tests/xsf_tests/test_hyp2f1.cpp
+++ tests/xsf_tests/test_hyp2f1.cpp
@@ -1,4 +1,6 @@
 #include "../testing_utils.h"
+#include <cmath>
+#include <limits>
 #include <tuple>
 #include <xsf/hyp2f1.h>
 
@@ -36,4 +38,14 @@ TEST_CASE("hyp2f1(-1, b, c, z) / gh-4446", "[hyp2f1][xsf_tests]") {
             }
         }
     }
+}
+
+
+TEST_CASE("hyp2f1(nan, nan, nan, complex('nan')) / gh-94", "[hyp2f1][xsf_tests]") {
+    double a = std::numeric_limits<double>::quiet_NaN();
+    std::complex<double> z = {std::numeric_limits<double>::quiet_NaN(), 0.0};
+
+    std::complex<double> result = xsf::hyp2f1(a, a, a, z);
+    REQUIRE(std::isnan(result.real()));
+    REQUIRE(std::isnan(result.imag()));
 }
