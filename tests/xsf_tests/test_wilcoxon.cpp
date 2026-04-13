58b07ef4f0eabca79cd687bd30f018d2aadc4eac
fbourgey <bourgeyflorian@gmail.com>
2026-04-13 16:58:01 -0400

    STYLE: clang-format

--- tests/xsf_tests/test_wilcoxon.cpp
+++ tests/xsf_tests/test_wilcoxon.cpp
@@ -19,14 +19,14 @@ TEST_CASE("wilcoxon_cdf exact small cases", "[wilcoxon_cdf]") {
         REQUIRE(xsf::wilcoxon_cdf(0.0, cdf_table, 0) == 1.0);
     }
 
-    {   // n = 1
+    { // n = 1
         const std::vector<double> pmf = xsf::wilcoxon_pmf(1);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_cdf(0.0, cdf_table, 1) == 0.5);
         REQUIRE(xsf::wilcoxon_cdf(1.0, cdf_table, 1) == 1.0);
     }
 
-    {   // n = 2
+    { // n = 2
         const std::vector<double> pmf = xsf::wilcoxon_pmf(2);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_cdf(1.0, cdf_table, 2) == 0.5);
@@ -35,7 +35,7 @@ TEST_CASE("wilcoxon_cdf exact small cases", "[wilcoxon_cdf]") {
         REQUIRE(xsf::wilcoxon_cdf(4.0, cdf_table, 2) == 1.0);
     }
 
-    {   // n = 3
+    { // n = 3
         const std::vector<double> pmf = xsf::wilcoxon_pmf(3);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_cdf(3.0, cdf_table, 3) == 0.625);
@@ -45,7 +45,7 @@ TEST_CASE("wilcoxon_cdf exact small cases", "[wilcoxon_cdf]") {
         REQUIRE(xsf::wilcoxon_cdf(7.0, cdf_table, 3) == 1.0);
     }
 
-    {   // n = 5
+    { // n = 5
         const std::vector<double> pmf = xsf::wilcoxon_pmf(5);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_cdf(5.0, cdf_table, 5) == 0.3125);
@@ -78,7 +78,7 @@ TEST_CASE("wilcoxon_sf exact small cases", "[wilcoxon_sf]") {
         REQUIRE(xsf::wilcoxon_sf(1.0, cdf_table, 0) == 0.0);
     }
 
-    {   // n = 1
+    { // n = 1
         const std::vector<double> pmf = xsf::wilcoxon_pmf(1);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_sf(0.0, cdf_table, 1) == 1.0);
@@ -86,7 +86,7 @@ TEST_CASE("wilcoxon_sf exact small cases", "[wilcoxon_sf]") {
         REQUIRE(xsf::wilcoxon_sf(2.0, cdf_table, 1) == 0.0);
     }
 
-    {   // n = 2
+    { // n = 2
         const std::vector<double> pmf = xsf::wilcoxon_pmf(2);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_sf(0.0, cdf_table, 2) == 1.0);
@@ -96,7 +96,7 @@ TEST_CASE("wilcoxon_sf exact small cases", "[wilcoxon_sf]") {
         REQUIRE(xsf::wilcoxon_sf(4.0, cdf_table, 2) == 0.0);
     }
 
-    {   // n = 3
+    { // n = 3
         const std::vector<double> pmf = xsf::wilcoxon_pmf(3);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_sf(3.0, cdf_table, 3) == 0.625);
@@ -106,7 +106,7 @@ TEST_CASE("wilcoxon_sf exact small cases", "[wilcoxon_sf]") {
         REQUIRE(xsf::wilcoxon_sf(7.0, cdf_table, 3) == 0.0);
     }
 
-    {   // n = 5
+    { // n = 5
         const std::vector<double> pmf = xsf::wilcoxon_pmf(5);
         const std::vector<double> cdf_table = xsf::wilcoxon_cdf_table(pmf);
         REQUIRE(xsf::wilcoxon_sf(5.0, cdf_table, 5) == 0.78125);
