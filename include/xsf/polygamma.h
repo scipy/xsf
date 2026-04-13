56487f01cfdf74df2931ad4436647e4563d484f5
lorenzo <lorenzo@quantummotion.tech>
2026-04-13 10:46:32 +0100

    Getting closer. There is still one error to pinpoint.

--- include/xsf/polygamma.h
+++ include/xsf/polygamma.h
@@ -57,7 +57,7 @@ namespace detail {
         * Higher order converge slower, so we need slightly more Bernoulli 
         * numbers than the digamma (Bernoulli numbers calculated via mpmath).
         */
-        static constexpr std::array<double, 48> bernoulli2k = {
+        static constexpr std::array<double, 36> bernoulli2k = {
             0.16666666666666665741e+0, -0.033333333333333332871e+0, 0.023809523809523808202e+0, 
             -0.033333333333333332871e+0, 0.075757575757575759678e+0, -0.25311355311355310249e+0, 
             1.1666666666666667407e+0, -7.0921568627450977118e+0, 54.971177944862155584e+0, 
@@ -70,14 +70,10 @@ namespace detail {
             -2.8498769302450882361e+30, 2.3865427499683627448e+32, -2.1399949257225334859e+34, 
             2.050097572347809739e+36, -2.0938005911346379301e+38, 2.2752696488463514863e+40, 
             -2.6257710286239577207e+42, 3.2125082102718031743e+44, -4.1598278166794711978e+46, 
-            5.6920695482035283174e+48, -8.2183629419784577665e+50, 1.2502904327166994004e+53, 
-            -2.0015583233248370052e+55, 3.3674982915364375556e+57, -5.9470970503135450205e+59, 
-            1.1011910323627976762e+62, -2.1355259545253502079e+64, 4.3328896986641193847e+66, 
-            -9.1885528241669331811e+68, 2.0346896776329073708e+71, -4.7003833958035730158e+73,
         };
 
         std::complex<double> res, term, fac_coeff;
-        std::complex<double> const zn_inv = std::pow(z, static_cast<std::complex<double>>(-n));
+        std::complex<double> const zn_inv = std::pow(z, static_cast<double>(-n));
         std::complex<double> const rzz = 1. / z / z;
         std::complex<double> zfac = zn_inv;
 
@@ -86,20 +82,25 @@ namespace detail {
         fac_coeff = (0.5 * static_cast<std::complex<double>>(n+1));
 
         for (int k=1;k<bernoulli2k.size()+1;k++) {
+
             zfac *= rzz;
-            fac_coeff *= static_cast<std::complex<double>>((2*k+n-2)*(2*k+n-1))/static_cast<std::complex<double>>(2*k*(2*k-1));
-            term = bernoulli2k[k-1] * fac_coeff *zfac;
+            term = bernoulli2k[k-1] * fac_coeff * zfac;
             res += term;
+
             if (std::abs(term) < std::numeric_limits<double>::epsilon() * std::abs(res)) {
                 break;
             }
+
+            double const num_coeff = (2.*k + n)*(2.*k + n + 1.);
+            double const den_coeff = (2.*k + 2) * (2.*k + 1.);
+            fac_coeff *= num_coeff / den_coeff;
         }
 
         return -res;
     }
 
     XSF_HOST_DEVICE inline std::complex<double> 
-    polygamma_reflection_rhs_nleq15(int n, std::complex<double> z) {
+    polygamma_reflection_rhs_nleq25(int n, std::complex<double> z) {
         /* The reflection formula for the polygamma of arbitrary positive order 
          * reads (http://dlmf.nist.gov/5.15.E6)
          *   psi(n, 1-z) + (-1)**(n-1) * psi(n, z) = (-1)**(n-1) * pi * d^n/dz^n cot(pi z)
@@ -110,44 +111,44 @@ namespace detail {
          * that satisfy the recurrence relation
          *   c_{n+1, m} = -(m+1) c_{n, m+1} - (m-1) c_{n, m-1}
          * This allows fast calculation of the coefficients of P_n(y).
-         * However, the coefficients blow up very rapidly in n, reaching ~1E+13 for n=15.
+         * However, this requires us to loop through the calculation n times.
          * For larger n, there is little point using this strategy. 
          * Recurrence should be used instead.
-         * Alternatively, we are going to divide this result by n!. So one could divide by (k+1)
-         * within the loop. However, one would need to consider a dynamical allocation strategy.
+         * Moreover, allowing a maximum n allows us to statically allocate
+         * the coefficient buffers and avoid any dynamical allocation.
          */
         
-        std::complex<double> cot_piz = 1. / std::tan(M_PI * z);
-        if (n == 0) {
-            return M_PI * cot_piz;
-        }
-
-        if(n>15){
-            // We are not going to use it for n>15.
+        if(n>25){
+            // We are not going to use it for n>25.
             // Statically allocate the memory and check against misuse
             return std::numeric_limits<double>::quiet_NaN();
         }
 
-        std::array<std::complex<double>, 17> coefficient_array{}; // all zeros at the start
-        std::array<std::complex<double>, 17> next_coeffs; 
+        std::array<double, 27> coefficient_array{}; // all zeros at the start
+        std::array<double, 27> next_coeffs; 
 
         coefficient_array[1] = 1.; // P_0(y) = y
 
+        /* The coefficients blow up very rapidly roughly O(n!). So we compute the 
+         * normalized RHS (-1)**(n-1) * pi * d^n/dz^n cot(pi z) / n!, and let the 
+         * general prefactor in the main polygamma driver handle the n! scaling
+         */
         for(int k=0; k<n; k++){
             std::fill(next_coeffs.begin(), next_coeffs.end(), 0.);
             for(int m=0; m<k+3; m++){
-                std::complex<double> term1 = (m + 1 <= k + 1) ? 
-                    (-static_cast<std::complex<double>>(m + 1) * coefficient_array[m + 1]) :
+                double const term1 = (m + 1 <= k + 1) ? 
+                    (-static_cast<double>(m + 1) * coefficient_array[m + 1]) :
                     0.;
-                std::complex<double> term2 = (m - 1 >= 0) ? 
-                    (-static_cast<std::complex<double>>(m - 1) * coefficient_array[m - 1]) :
+                double const term2 = (m - 1 >= 0) ? 
+                    (-static_cast<double>(m - 1) * coefficient_array[m - 1]) :
                     0.;
-                next_coeffs[m] = term1 + term2;
+                next_coeffs[m] = (term1 + term2)/static_cast<double>(k + 1);
             }
             std::copy(next_coeffs.begin(), next_coeffs.end(), coefficient_array.begin());
         }
 
         std::complex<double> result = 0.0;
+        std::complex<double> cot_piz = cospi(z) / sinpi(z);
         // Evaluate from highest degree down to 0
         for (int m = n + 1; m >= 0; m--) {
             result = result * cot_piz + coefficient_array[m];
@@ -248,11 +249,11 @@ XSF_HOST_DEVICE inline std::complex<double> polygamma(int n, std::complex<double
         return {std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()};
     }
 
-    if (n <= 15){ 
+    if (n <= 25){ 
         // For small orders, we can use the reflection formula
         if(z.real() < 0 && std::abs(z.imag()) < smallabsz) {
-            std::complex<double> const reflection_rhs = detail::polygamma_reflection_rhs_nleq15(n, z);
-            res -= reflection_rhs / gamma(static_cast<double>(n + 1));
+            std::complex<double> const reflection_rhs = detail::polygamma_reflection_rhs_nleq25(n, z);
+            res -= reflection_rhs;
             sign *= (n & 1) ? -1. : 1.;
             z = 1. - z;
             absz = std::abs(z);
