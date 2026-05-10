cd6a277a41dd858fb2b831d7bf57ba7210ba5a03
steppi <1953382+steppi@users.noreply.github.com>
2026-05-09 22:34:16 -0400

    Drop as_mdspan from numpy.h

--- include/xsf/numpy.h
+++ include/xsf/numpy.h
@@ -62,17 +62,6 @@ namespace numpy {
 
     } // namespace detail
 
-    // Helper to wrap a 1D std::vector in a contiguous mdspan
-    template <typename T>
-    auto as_mdspan(std::vector<T>& vec) {
-        return std::mdspan<T, std::dextents<ptrdiff_t, 1>>(vec.data(), vec.size());
-    }
-
-    template <typename T>
-    auto as_mdspan(const std::vector<T>& vec) {
-        return std::mdspan<const T, std::dextents<ptrdiff_t, 1>>(vec.data(), vec.size());
-    }
-
     using cfloat = std::complex<float>;
     using cdouble = std::complex<double>;
 
