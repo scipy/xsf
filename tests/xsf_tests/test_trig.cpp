c8958487efc2df8d4a8a5178ae5dd9911b3f504f
Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>
2026-05-21 17:34:57 +0100

    MAINT: ensure zero sign is returned for tangd at multiples of 180 and cotdp at multiples of 90 (#153)

--- tests/xsf_tests/test_trig.cpp
+++ tests/xsf_tests/test_trig.cpp
@@ -12,8 +12,64 @@ void check_zeros(double (*f)(double)) {
     }
 }
 
-TEST_CASE("sindg IEEE scipy/20731", "[sindg][xsf_tests]") { check_zeros(&xsf::sindg); }
+TEST_CASE("sindg IEEE pm zero scipy/20731", "[sindg][xsf_tests]") { check_zeros(&xsf::sindg); }
 
-TEST_CASE("sinpi IEEE scipy/20731", "[sinpi][xsf_tests]") { check_zeros(&xsf::sinpi); }
+TEST_CASE("sinpi IEEE pm zero scipy/20731", "[sinpi][xsf_tests]") { check_zeros(&xsf::sinpi); }
 
-TEST_CASE("tandg IEEE scipy/20731", "[tandg][xsf_tests]") { check_zeros(&xsf::tandg); }
+TEST_CASE("tandg IEEE pm zero scipy/20731", "[tandg][xsf_tests]") { check_zeros(&xsf::tandg); }
+
+TEST_CASE("tandg IEEE zero sign at multiples of 180 scipy/20731", "[tandg][xsf_tests]") {
+    double x, result;
+    for (int n : {1, 2}) {
+        x = (2 * n + 1) * 180.0;
+        result = xsf::tandg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(std::signbit(result));
+        REQUIRE(result == 0);
+        x = (2 * n + 2) * 180.0;
+        result = xsf::tandg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(!std::signbit(result));
+        REQUIRE(result == 0);
+    }
+    for (int n : {-1, -2}) {
+        x = (2 * n - 1) * 180.0;
+        result = xsf::tandg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(!std::signbit(result));
+        REQUIRE(result == 0);
+        x = (2 * n - 2) * 180.0;
+        result = xsf::tandg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(std::signbit(result));
+        REQUIRE(result == 0);
+    }
+}
+
+TEST_CASE("cotdg IEEE zero sign at multiples of 90 scipy/20731", "[cotdg][xsf_tests]") {
+    double x, result;
+    for (int n : {1, 2}) {
+        x = (4 * n - 3) * 90.0;
+        result = xsf::cotdg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(!std::signbit(result));
+        REQUIRE(result == 0);
+        x = (4 * n - 1) * 90.0;
+        result = xsf::cotdg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(std::signbit(result));
+        REQUIRE(result == 0);
+    }
+    for (int n : {-1, -2}) {
+        x = (4 * n + 3) * 90.0;
+        result = xsf::cotdg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(std::signbit(result));
+        REQUIRE(result == 0);
+        x = (4 * n + 1) * 90.0;
+        result = xsf::cotdg(x);
+        CAPTURE(n, x, result);
+        REQUIRE(!std::signbit(result));
+        REQUIRE(result == 0);
+    }
+}
