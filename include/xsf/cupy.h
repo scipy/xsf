103af1d240cf55114aeb931552d8a3df0f492d7a
steppi <1953382+steppi@users.noreply.github.com>
2026-05-22 11:30:16 -0400

    only use libcu++ headers in cupy.h

--- include/xsf/cupy.h
+++ include/xsf/cupy.h
@@ -1,24 +1,25 @@
 #pragma once
 
-#include "config.h"
-#include "third_party/kokkos/mdspan.hpp"
+#include <cuda/std/cstddef>
+#include <cuda/std/array>
+#include <cuda/std/mdspan>
 
 namespace xsf {
 
 template <typename T, int ndim, int is_c_contiguous, int index_32_bits, int core_ndim>
 __device__ inline auto as_mdspan(const CArray<T, ndim, is_c_contiguous, index_32_bits, core_ndim>& arr) {
-    std::array<std::ptrdiff_t, ndim> exts;
-    std::array<std::ptrdiff_t, ndim> strs;
+    cuda::std::array<cuda::std::ptrdiff_t, ndim> exts;
+    cuda::std::array<cuda::std::ptrdiff_t, ndim> strs;
 
     for (int i = 0; i < ndim; ++i) {
-        exts[i] = static_cast<std::ptrdiff_t>(arr.shape_[i]);
-        strs[i] = static_cast<std::ptrdiff_t>(arr.strides_[i]) / sizeof(T);
+        exts[i] = static_cast<cuda::std::ptrdiff_t>(arr.shape_[i]);
+        strs[i] = static_cast<cuda::std::ptrdiff_t>(arr.strides_[i]) / sizeof(T);
     }
 
-    using Extents = std::dextents<std::ptrdiff_t, ndim>;
-    using Mapping = std::layout_stride::mapping<Extents>;
+    using Extents = cuda::std::dextents<cuda::std::ptrdiff_t, ndim>;
+    using Mapping = cuda::std::layout_stride::mapping<Extents>;
 
-    return std::mdspan<T, Extents, std::layout_stride>(
+    return cuda::std::mdspan<T, Extents, cuda::std::layout_stride>(
         arr.data_, Mapping(Extents(exts), strs)
     );
 }
