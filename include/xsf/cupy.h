46d42a29f30f6c46c1b0ca28a4d8788b93f2dd2d
steppi <1953382+steppi@users.noreply.github.com>
2026-05-22 12:40:03 -0400

    fix cupy as_mdspan

--- include/xsf/cupy.h
+++ include/xsf/cupy.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <cuda/std/cstddef>
 #include <cuda/std/array>
+#include <cuda/std/cstddef>
 #include <cuda/std/mdspan>
 
 namespace xsf {
 
 template <typename T, int ndim, bool is_c_contiguous, bool index_32_bits, int core_ndim>
-__device__ inline auto as_mdspan(const CArray<T, ndim, is_c_contiguous, index_32_bits, core_ndim>& arr) {
+__device__ inline auto as_mdspan(CArray<T, ndim, is_c_contiguous, index_32_bits, core_ndim> arr) {
     cuda::std::array<cuda::std::ptrdiff_t, ndim> exts;
     cuda::std::array<cuda::std::ptrdiff_t, ndim> strs;
 
@@ -19,9 +19,7 @@ __device__ inline auto as_mdspan(const CArray<T, ndim, is_c_contiguous, index_32
     using Extents = cuda::std::dextents<cuda::std::ptrdiff_t, ndim>;
     using Mapping = cuda::std::layout_stride::mapping<Extents>;
 
-    return cuda::std::mdspan<T, Extents, cuda::std::layout_stride>(
-        arr.data_, Mapping(Extents(exts), strs)
-    );
+    return cuda::std::mdspan<T, Extents, cuda::std::layout_stride>(arr.data(), Mapping(Extents(exts), strs));
 }
 
 } // namespace xsf
