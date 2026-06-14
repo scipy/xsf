77ee5e305aeda9242d2aa4715a932a3631255b25
Jeff Epler <jepler@unpythonic.net>
2026-06-13 20:14:03 -0500

    trig: Ensure all literals have correct type in sinpi/cospi
    This avoids excess precision, as well as using a more accurate
    pi value for long double type arguments.

--- include/xsf/cephes/trig.h
+++ include/xsf/cephes/trig.h
@@ -12,50 +12,47 @@
 #pragma once
 
 #include "../config.h"
+#include "numbers.h"
 
 namespace xsf {
 namespace cephes {
 
     /* Compute acos(x) / pi */
-    template<typename T>
-    XSF_HOST_DEVICE T acospi(double x)
-    {  
-        double r = std::acos(x)/M_PI;
-        if (std::isgreater(r, 1.0)) {  
-            return 1.0;
+    template <typename T>
+    XSF_HOST_DEVICE T acospi(double x) {
+        double r = std::acos(x) / pi_v<T>;
+        if (std::isgreater(r, T(1.0))) {
+            return T(1.0);
         }
         return r;
     }
 
     /* Compute asin(x) / pi */
-    template<typename T>
-    XSF_HOST_DEVICE T asinpi(double x)
-    {  
-        double r = std::asin(x)/M_PI;
-        if (std::isgreater(r, 1.0)) {  
-            return 1.0;
+    template <typename T>
+    XSF_HOST_DEVICE T asinpi(double x) {
+        double r = std::asin(x) / pi_v<T>;
+        if (std::isgreater(r, T(1.0))) {
+            return T(1.0);
         }
         return r;
     }
 
     /* Compute atan(x) / pi */
-    template<typename T>
-    XSF_HOST_DEVICE T atanpi(T x)
-    {
-        double r = std::atan(x)/M_PI;
-        if (isgreater(std::fabs(r), 0.5)) {
-            return std::copysign(0.5, r);
+    template <typename T>
+    XSF_HOST_DEVICE T atanpi(T x) {
+        double r = std::atan(x) / pi_v<T>;
+        if (isgreater(std::fabs(r), T(0.5))) {
+            return std::copysign(T(0.5), r);
         }
         return r;
     }
 
     /* Compute atan2(y, x) / pi */
-    template<typename T>
-    XSF_HOST_DEVICE T atan2pi(double y, double x)
-    {
-        double r = std::atan2(y, x)/M_PI;
-        if (std::isgreater(std::fabs(r), 1.0)) {
-            return std::copysign(1.0, r);
+    template <typename T>
+    XSF_HOST_DEVICE T atan2pi(double y, double x) {
+        double r = std::atan2(y, x) / pi_v<T>;
+        if (std::isgreater(std::fabs(r), T(1.0))) {
+            return std::copysign(T(1.0), r);
         }
         return r;
     }
@@ -63,69 +60,69 @@ namespace cephes {
     /* Compute sin(pi * x). */
     template <typename T>
     XSF_HOST_DEVICE T sinpi(T x) {
-        T s = 1.0;
+        T s = T(1.0);
 
         if (std::signbit(x)) {
             x = -x;
-            s = -1.0;
+            s = -T(1.0);
         }
 
-        T r = std::fmod(x, 2.0);
-        if (r < 0.5) {
-            return s * std::sin(M_PI * r);
-        } else if (r > 1.5) {
-            return s * std::sin(M_PI * (r - 2.0));
+        T r = std::fmod(x, T(2.0));
+        if (r < T(0.5)) {
+            return s * std::sin(pi_v<T> * r);
+        } else if (r > T(1.5)) {
+            return s * std::sin(pi_v<T> * (r - T(2.0)));
         } else {
-            return -s * std::sin(M_PI * (r - 1.0));
+            return -s * std::sin(pi_v<T> * (r - T(1.0)));
         }
     }
 
     /* Compute cos(pi * x) */
     template <typename T>
     XSF_HOST_DEVICE T cospi(T x) {
-        if (x < 0.0) {
+        if (x < T(0.0)) {
             x = -x;
         }
 
-        T r = std::fmod(x, 2.0);
-        if (r == 0.5) {
-            // We don't want to return -0.0
-            return 0.0;
+        T r = std::fmod(x, T(2.0));
+        if (r == T(0.5)) {
+            // We don't want to return -T(0.0)
+            return T(0.0);
         }
-        if (r < 1.0) {
-            return -std::sin(M_PI * (r - 0.5));
+        if (r < T(1.0)) {
+            return -std::sin(pi_v<T> * (r - T(0.5)));
         } else {
-            return std::sin(M_PI * (r - 1.5));
+            return std::sin(pi_v<T> * (r - T(1.5)));
         }
     }
 
     /* Compute tan(pi * x). */
     template <typename T>
     XSF_HOST_DEVICE T tanpi(T x) {
-        T y, absy;         
+        T y, absy;
         if (!std::isfinite(x)) {
             return std::tan(x);
         }
-        y = x - 2.0 * std::round(0.5 * x);
+        y = x - T(2.0) * std::round(T(0.5) * x);
         absy = std::fabs(y);
-        if (absy == 0.0) {
-            return std::copysign(0.0, x);
-        } 
-        if (absy == 1.0) {
-            return std::copysign(0.0, -x);
+        if (absy == T(0.0)) {
+            return std::copysign(T(0.0), x);
         }
-        if (absy == 0.5) {
+        if (absy == T(1.0)) {
+            return std::copysign(T(0.0), -x);
+        }
+        if (absy == T(0.5)) {
             errno = ERANGE;
-            return 1.0 / std::copysign(0.0, y);
+            return T(1.0) / std::copysign(T(0.0), y);
         }
-        if (absy > 0.5) {
-            y -= std::copysign(1.0, y);
+        if (absy > T(0.5)) {
+            y -= std::copysign(T(1.0), y);
             absy = std::fabs(y);
         }
-        if (absy <= 0.25) {
-            return std::tan(M_PI * y);
+        if (absy <= T(0.25)) {
+            return std::tan(pi_v<T> * y);
         }
-        return std::copysign(1.0 / std::tan(M_PI * (0.5 - absy)), y);
+        return std::copysign(T(1.0) / std::tan(pi_v<T> * (T(0.5) - absy)), y);
     }
 
 } // namespace cephes
