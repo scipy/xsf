5759f206993cc2c34301e13ecb656446d5e79e45
Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>
2026-06-26 19:57:04 +0100

    MAINT: simplify wrightomega (#207)

--- include/xsf/wright.h
+++ include/xsf/wright.h
@@ -47,24 +47,6 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-/**********************************************************************/
-/* wrightomega_ext is the extended routine for evaluating the wright  */
-/* omega function with the option of extracting the last update step, */
-/* and the penultimate residual.        */
-/*                                                                    */
-/* Calling:                                                           */
-/*   success = wrightomega_ext(z,w,e,r,cond);                         */
-/*                                                                    */
-/* Input:                                                             */
-/*   z  --  double complex                                            */
-/*          Value to evaluate Wrightomega(z) at.                      */
-/*                                                                    */
-/*   w  --  double complex*                                           */
-/*          Pointer to return value of Wrightomega(z).                */
-/*                                                                    */
-/* Output: returns 0 on successful exit.                               */
-/**********************************************************************/
-
 #pragma once
 
 #include "config.h"
@@ -73,7 +55,6 @@
 namespace xsf {
 namespace detail {
 
-    constexpr std::complex<double> I(0.0, 1.0);
     constexpr double TWOITERTOL = std::numeric_limits<double>::epsilon();
 
     /* Computes fl(a+b) and err(a+b).  */
@@ -118,234 +99,220 @@ namespace detail {
         }
     }
 
-    XSF_HOST_DEVICE inline int wrightomega_ext(std::complex<double> z, std::complex<double> *w) {
-        double pi = M_PI, s = 1.0;
-        double x, y, ympi, yppi, near;
-        std::complex<double> e, r, pz, wp1, t, fac;
-
-        /* extract real and imaginary parts of z */
-        x = std::real(z);
-        y = std::imag(z);
-
-        /* compute if we are near the branch cuts */
-        ympi = y - pi;
-        yppi = y + pi;
-        near = 0.1e-1;
-
-        /* Test for floating point exceptions */
-        /*****************************/
-        /* NaN output for NaN input  */
-        /*****************************/
-        if (std::isnan(x) || std::isnan(y)) {
-            *w = std::complex<double>(
-                std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()
-            );
-            return 0;
-        }
-        /*********************************/
-        /* Signed zeros between branches */
-        /*********************************/
-        else if (std::isinf(x) && (x < 0.0) && (-pi < y) && (y <= pi)) {
-            if (std::fabs(y) <= pi / 2.0) {
-                if (y >= 0) {
-                    *w = std::complex<double>(0.0, 0.0);
-                } else {
-                    *w = std::complex<double>(0.0, -0.0);
-                }
-            } else {
-                if (y >= 0) {
-                    *w = std::complex<double>(-0.0, 0.0);
-                } else {
-                    *w = std::complex<double>(-0.0, -0.0);
-                }
-            }
-            return 0;
-        }
-        /**************************/
-        /* Asymptotic for large z */
-        /**************************/
-        else if (std::isinf(x) || std::isinf(y)) {
-            *w = std::complex<double>(x, y);
-            return 0;
-        }
-
-        /******************************************/
-        /* Test If exactly on the singular points */
-        /******************************************/
-        if ((x == -1.0) && (std::fabs(y) == pi)) {
-            *w = std::complex<double>(-1.0, 0.0);
-            return 0;
-        }
-
-        /* Choose approximation based on region */
-        /**********************************/
-        /* Region 1: upper branch point   */
-        /* Series about z=-1+Pi*I         */
-        /**********************************/
-        if ((-2.0 < x && x <= 1.0 && 1.0 < y && y < 2.0 * pi)) {
-            pz = std::conj(std::sqrt(std::conj(2.0 * (z + 1.0 - I * pi))));
-            *w = -1.0 +
-                 (I + (1.0 / 3.0 + (-1.0 / 36.0 * I + (1.0 / 270.0 + 1.0 / 4320.0 * I * pz) * pz) * pz) * pz) * pz;
-        }
-        /**********************************/
-        /* Region 2: lower branch point   */
-        /* Series about z=-1-Pi*I         */
-        /**********************************/
-        else if ((-2.0 < x && x <= 1.0 && -2.0 * pi < y && y < -1.0)) {
-            pz = std::conj(std::sqrt(std::conj(2.0 * (z + 1.0 + I * pi))));
-            *w = -1.0 +
-                 (-I + (1.0 / 3.0 + (1.0 / 36.0 * I + (1.0 / 270.0 - 1.0 / 4320.0 * I * pz) * pz) * pz) * pz) * pz;
-        }
-        /*********************************/
-        /* Region 3: between branch cuts */
-        /* Series: About -infinity       */
-        /*********************************/
-        else if (x <= -2.0 && -pi < y && y <= pi) {
-            pz = std::exp(z);
-            *w = (1.0 + (-1.0 + (3.0 / 2.0 + (-8.0 / 3.0 + 125.0 / 24.0 * pz) * pz) * pz) * pz) * pz;
-            if (*w == 0.0) {
-                set_error("wrightomega", SF_ERROR_UNDERFLOW, "underflow in exponential series");
-                /* Skip the iterative scheme because it computes log(*w) */
-                return 0;
-            }
-        }
-        /**********************/
-        /* Region 4: Mushroom */
-        /* Series about z=1   */
-        /**********************/
-        else if (
-            ((-2.0 < x) && (x <= 1.0) && (-1.0 <= y) && (y <= 1.0)) ||
-            ((-2.0 < x) && (x - 0.1e1) * (x - 0.1e1) + y * y <= pi * pi)
-        ) {
-            pz = z - 1.0;
-            *w = 1.0 / 2.0 + 1.0 / 2.0 * z +
-                 (1.0 / 16.0 + (-1.0 / 192.0 + (-1.0 / 3072.0 + 13.0 / 61440.0 * pz) * pz) * pz) * pz * pz;
-        }
-        /*************************/
-        /* Region 5: Top wing    */
-        /* Negative log series   */
-        /*************************/
-        else if (x <= -0.105e1 && pi < y && y - pi <= -0.75e0 * (x + 0.1e1)) {
-            t = z - I * pi;
-            pz = std::log(-t);
-            *w = t - pz;
-            fac = pz / t;
-            *w += fac;
-            fac /= t;
-            *w += fac * (0.5 * pz - 1.0);
-            fac /= t;
-            *w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
-            if (std::abs(z) > 1e50)
-            /* Series is accurate and the iterative scheme could overflow */
-            {
-                return 0;
-            }
-        }
-        /***************************/
-        /* Region 6: Bottom wing   */
-        /* Negative log series     */
-        /***************************/
-        else if (x <= -0.105e1 && 0.75e0 * (x + 0.1e1) < y + pi && y + pi <= 0.0e0) {
-            t = z + I * pi;
-            pz = std::log(-t);
-            *w = t - pz;
-            fac = pz / t;
-            *w += fac;
-            fac /= t;
-            *w += fac * (0.5 * pz - 1.0);
-            fac /= t;
-            *w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
-            if (std::abs(z) > 1e50)
-            /* Series is accurate and the iterative scheme could overflow */
-            {
-                return 0;
-            }
-        }
-        /************************************/
-        /* Region 7: Everywhere else        */
-        /* Series solution about infinity   */
-        /************************************/
-        else {
-            pz = std::log(z);
-            *w = z - pz;
-            fac = pz / z;
-            *w += fac;
-            fac /= z;
-            *w += fac * (0.5 * pz - 1.0);
-            fac /= z;
-            *w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
-            if (std::abs(z) > 1e50)
-            /* Series is accurate and the iterative scheme could overflow */
-            {
-                return 0;
-            }
-        }
-
-        /**********************************/
-        /* Regularize if near branch cuts */
-        /**********************************/
-        if (x <= -0.1e1 + near && (std::fabs(ympi) <= near || std::fabs(yppi) <= near)) {
-            s = -1.0;
-            if (fabs(ympi) <= near) {
-                /* Recompute ympi with directed rounding */
-                ympi = detail::add_round_up(y, -pi);
-
-                if (ympi <= 0.0) {
-                    ympi = detail::add_round_down(y, -pi);
-                }
-
-                z = x + I * ympi;
-            } else {
-                /* Recompute yppi with directed rounding */
-                yppi = detail::add_round_up(y, pi);
-
-                if (yppi <= 0.0) {
-                    yppi = detail::add_round_down(y, pi);
-                }
-
-                z = x + I * yppi;
-            }
-        }
-
-        /*****************/
-        /* Iteration one */
-        /*****************/
-        *w = s * *w;
-        r = z - s * *w - std::log(*w);
-        wp1 = s * *w + 1.0;
-        e = r / wp1 * (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - r) / (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - 2.0 * r);
-        *w = *w * (1.0 + e);
-
-        /*****************/
-        /* Iteration two */
-        /*****************/
-        if (std::abs((2.0 * *w * *w - 8.0 * *w - 1.0) * pow(abs(r), 4.0)) >=
-            detail::TWOITERTOL * 72.0 * std::pow(std::abs(wp1), 6.0)) {
-            r = z - s * *w - std::log(*w);
-            wp1 = s * *w + 1.0;
-            e = r / wp1 * (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - r) / (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - 2.0 * r);
-            *w = *w * (1.0 + e);
-        }
-
-        /***********************/
-        /* Undo regularization */
-        /***********************/
-        *w = s * *w;
-
-        return 0;
-    }
 } // namespace detail
 
 XSF_HOST_DEVICE inline std::complex<double> wrightomega(std::complex<double> z) {
-    std::complex<double> w;
-    detail::wrightomega_ext(z, &w);
+    constexpr std::complex<double> I(0.0, 1.0);
+    double pi = M_PI, s = 1.0;
+    double x, y, ympi, yppi, near;
+    std::complex<double> e, r, pz, wp1, t, fac, w;
+
+    /* extract real and imaginary parts of z */
+    x = std::real(z);
+    y = std::imag(z);
+
+    /* compute if we are near the branch cuts */
+    ympi = y - pi;
+    yppi = y + pi;
+    near = 0.1e-1;
+
+    /* Test for floating point exceptions */
+    /*****************************/
+    /* NaN output for NaN input  */
+    /*****************************/
+    if (std::isnan(x) || std::isnan(y)) {
+        return std::complex<double>(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN());
+    }
+    /*********************************/
+    /* Signed zeros between branches */
+    /*********************************/
+    else if (std::isinf(x) && (x < 0.0) && (-pi < y) && (y <= pi)) {
+        if (std::fabs(y) <= pi / 2.0) {
+            if (y >= 0) {
+                return std::complex<double>(0.0, 0.0);
+            } else {
+                return std::complex<double>(0.0, -0.0);
+            }
+        } else {
+            if (y >= 0) {
+                return std::complex<double>(-0.0, 0.0);
+            } else {
+                return std::complex<double>(-0.0, -0.0);
+            }
+        }
+    }
+    /**************************/
+    /* Asymptotic for large z */
+    /**************************/
+    else if (std::isinf(x) || std::isinf(y)) {
+        return std::complex<double>(x, y);
+    }
+
+    /******************************************/
+    /* Test If exactly on the singular points */
+    /******************************************/
+    if ((x == -1.0) && (std::fabs(y) == pi)) {
+        return std::complex<double>(-1.0, 0.0);
+    }
+
+    /* Choose approximation based on region */
+    /**********************************/
+    /* Region 1: upper branch point   */
+    /* Series about z=-1+Pi*I         */
+    /**********************************/
+    if ((-2.0 < x && x <= 1.0 && 1.0 < y && y < 2.0 * pi)) {
+        pz = std::conj(std::sqrt(std::conj(2.0 * (z + 1.0 - I * pi))));
+        w = -1.0 + (I + (1.0 / 3.0 + (-1.0 / 36.0 * I + (1.0 / 270.0 + 1.0 / 4320.0 * I * pz) * pz) * pz) * pz) * pz;
+    }
+    /**********************************/
+    /* Region 2: lower branch point   */
+    /* Series about z=-1-Pi*I         */
+    /**********************************/
+    else if ((-2.0 < x && x <= 1.0 && -2.0 * pi < y && y < -1.0)) {
+        pz = std::conj(std::sqrt(std::conj(2.0 * (z + 1.0 + I * pi))));
+        w = -1.0 + (-I + (1.0 / 3.0 + (1.0 / 36.0 * I + (1.0 / 270.0 - 1.0 / 4320.0 * I * pz) * pz) * pz) * pz) * pz;
+    }
+    /*********************************/
+    /* Region 3: between branch cuts */
+    /* Series: About -infinity       */
+    /*********************************/
+    else if (x <= -2.0 && -pi < y && y <= pi) {
+        pz = std::exp(z);
+        w = (1.0 + (-1.0 + (3.0 / 2.0 + (-8.0 / 3.0 + 125.0 / 24.0 * pz) * pz) * pz) * pz) * pz;
+        if (w == 0.0) {
+            set_error("wrightomega", SF_ERROR_UNDERFLOW, "underflow in exponential series");
+            /* Skip the iterative scheme because it computes log(*w) */
+            return w;
+        }
+    }
+    /**********************/
+    /* Region 4: Mushroom */
+    /* Series about z=1   */
+    /**********************/
+    else if (
+        ((-2.0 < x) && (x <= 1.0) && (-1.0 <= y) && (y <= 1.0)) ||
+        ((-2.0 < x) && (x - 0.1e1) * (x - 0.1e1) + y * y <= pi * pi)
+    ) {
+        pz = z - 1.0;
+        w = 1.0 / 2.0 + 1.0 / 2.0 * z +
+            (1.0 / 16.0 + (-1.0 / 192.0 + (-1.0 / 3072.0 + 13.0 / 61440.0 * pz) * pz) * pz) * pz * pz;
+    }
+    /*************************/
+    /* Region 5: Top wing    */
+    /* Negative log series   */
+    /*************************/
+    else if (x <= -0.105e1 && pi < y && y - pi <= -0.75e0 * (x + 0.1e1)) {
+        t = z - I * pi;
+        pz = std::log(-t);
+        w = t - pz;
+        fac = pz / t;
+        w += fac;
+        fac /= t;
+        w += fac * (0.5 * pz - 1.0);
+        fac /= t;
+        w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
+        if (std::abs(z) > 1e50)
+        /* Series is accurate and the iterative scheme could overflow */
+        {
+            return w;
+        }
+    }
+    /***************************/
+    /* Region 6: Bottom wing   */
+    /* Negative log series     */
+    /***************************/
+    else if (x <= -0.105e1 && 0.75e0 * (x + 0.1e1) < y + pi && y + pi <= 0.0e0) {
+        t = z + I * pi;
+        pz = std::log(-t);
+        w = t - pz;
+        fac = pz / t;
+        w += fac;
+        fac /= t;
+        w += fac * (0.5 * pz - 1.0);
+        fac /= t;
+        w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
+        if (std::abs(z) > 1e50)
+        /* Series is accurate and the iterative scheme could overflow */
+        {
+            return w;
+        }
+    }
+    /************************************/
+    /* Region 7: Everywhere else        */
+    /* Series solution about infinity   */
+    /************************************/
+    else {
+        pz = std::log(z);
+        w = z - pz;
+        fac = pz / z;
+        w += fac;
+        fac /= z;
+        w += fac * (0.5 * pz - 1.0);
+        fac /= z;
+        w += fac * (pz * pz / 3.0 - 3.0 * pz / 2.0 + 1.0);
+        if (std::abs(z) > 1e50)
+        /* Series is accurate and the iterative scheme could overflow */
+        {
+            return w;
+        }
+    }
+
+    /**********************************/
+    /* Regularize if near branch cuts */
+    /**********************************/
+    if (x <= -0.1e1 + near && (std::fabs(ympi) <= near || std::fabs(yppi) <= near)) {
+        s = -1.0;
+        if (std::fabs(ympi) <= near) {
+            /* Recompute ympi with directed rounding */
+            ympi = detail::add_round_up(y, -pi);
+
+            if (ympi <= 0.0) {
+                ympi = detail::add_round_down(y, -pi);
+            }
+
+            z = x + I * ympi;
+        } else {
+            /* Recompute yppi with directed rounding */
+            yppi = detail::add_round_up(y, pi);
+
+            if (yppi <= 0.0) {
+                yppi = detail::add_round_down(y, pi);
+            }
+
+            z = x + I * yppi;
+        }
+    }
+
+    /*****************/
+    /* Iteration one */
+    /*****************/
+    w = s * w;
+    r = z - s * w - std::log(w);
+    wp1 = s * w + 1.0;
+    e = r / wp1 * (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - r) / (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - 2.0 * r);
+    w = w * (1.0 + e);
+
+    /*****************/
+    /* Iteration two */
+    /*****************/
+    if (std::abs((2.0 * w * w - 8.0 * w - 1.0) * std::pow(std::abs(r), 4.0)) >=
+        detail::TWOITERTOL * 72.0 * std::pow(std::abs(wp1), 6.0)) {
+        r = z - s * w - std::log(w);
+        wp1 = s * w + 1.0;
+        e = r / wp1 * (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - r) / (2.0 * wp1 * (wp1 + 2.0 / 3.0 * r) - 2.0 * r);
+        w = w * (1.0 + e);
+    }
+
+    /***********************/
+    /* Undo regularization */
+    /***********************/
+    w = s * w;
+
     return w;
 }
 
 XSF_HOST_DEVICE inline std::complex<float> wrightomega(std::complex<float> z) {
-    std::complex<double> w;
-    detail::wrightomega_ext(static_cast<std::complex<double>>(z), &w);
-    return static_cast<std::complex<float>>(w);
+    return static_cast<std::complex<float>>(wrightomega(static_cast<std::complex<double>>(z)));
 }
 
 XSF_HOST_DEVICE inline double wrightomega(double x) {
