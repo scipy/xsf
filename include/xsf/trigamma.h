d61bfd905a854db43c405166239d1e3eb42d180f
Jake Bowhay <jb9.bowhay@gmail.com>
2026-08-05 08:48:38 +0100

    wip

---
+++ include/xsf/trigamma.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "config.h"
+#include "cephes/polevl.h"
+#include "trig.h"
+#include "evalpoly.h"
+
+#include <complex>
+#include <type_traits>
+
+namespace xsf {
+
+// https://github.com/JuliaMath/SpecialFunctions.jl/blob/c9ff36085c9008b9bdaa457e745f459a1827e52e/src/gamma.jl#L65
+template <typename T>
+XSF_HOST_DEVICE inline T trigamma(T z) {
+    double x = std::real(z);
+    if (x <= 0) { // reflection formula
+        T s = xsf::sinpi(z);
+        T c = M_PI / s;
+        return c * c - trigamma(T(1) - z);
+    }
+
+    T psi = 0;
+    int N = 10;
+    int n;
+    if (x < N) {
+        n = N - std::floor(x);
+        psi += 1.0 / (z * z);
+        for (int v = 1; v < n; v++) {
+            T w = z + static_cast<T>(v);
+            psi += 1.0 / (w*w);
+        }
+        z += n;
+    }
+    T t = 1.0 / z;
+    T w = t * t;
+    psi += t + 0.5 * w;
+    double coeffs[] = {-7.092156862745098, 1.1666666666666667, -0.2531135531135531, 0.07575757575757576, -0.03333333333333333, 0.023809523809523808, -0.03333333333333333, 0.16666666666666666};
+    using base_t = std::remove_cv_t<T>;
+    if constexpr (std::is_same_v<base_t, std::complex<double>> || std::is_same_v<base_t, std::complex<float>>) {
+        psi += t * w * xsf::cevalpoly(coeffs, 7, std::complex<double>(w));
+    } else {
+        psi += t * w * xsf::cephes::polevl(w, coeffs, 7);
+    }
+    return psi;
+}
+
+} // namespace xsf
---
+++ tests/xsf_tests/test_trigamma.cpp
@@ -0,0 +1,27 @@
+#include "../testing_utils.h"
+#include <complex>
+#include <tuple>
+#include <xsf/trigamma.h>
+
+
+TEST_CASE("trigamma real", "[trigamma][xsf_tests]") {
+    using test_case = std::tuple<double, double, double>;
+
+    // Reference values from https://github.com/JuliaMath/SpecialFunctions.jl/blob/c9ff36085c9008b9bdaa457e745f459a1827e52e/test/gamma.jl#L30-L44.
+    auto [z, ref_trigamma, rtol] = GENERATE(
+        test_case{0.1, 101.433299150792758817, 1e-14},
+        test_case{0.5, M_PI*M_PI/2, 1e-14},
+        test_case{1.0, M_PI*M_PI/6, 1e-14},
+        test_case{2.0, M_PI*M_PI/6 - 1, 1e-14},
+        test_case{3.0, M_PI*M_PI/6 - 5.0/4, 1e-14},
+        test_case{4.0, M_PI*M_PI/6 - 49.0/36, 1e-14},
+        test_case{5.0, M_PI*M_PI/6 - 205.0/144, 1e-14},
+        test_case{10.0, M_PI*M_PI/6 - 1225.0/576, 1e-14}
+    );
+
+    double result_trigamma = xsf::trigamma(z);
+    double rel_err_trigamma = xsf::extended_relative_error(result_trigamma, ref_trigamma);
+
+    CAPTURE(z, result_trigamma, ref_trigamma, rel_err_trigamma, rtol);
+    REQUIRE(rel_err_trigamma <= rtol);
+}
