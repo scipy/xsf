4e663a6e21c39c63b0f5fafc9e2d6763121f8b15
Jake Bowhay <jb9.bowhay@gmail.com>
2026-09-09 00:04:47 +0100

    add some more comments

--- include/xsf/trigamma.h
+++ include/xsf/trigamma.h
@@ -37,10 +37,15 @@
 namespace xsf {
 
 // https://github.com/JuliaMath/SpecialFunctions.jl/blob/c9ff36085c9008b9bdaa457e745f459a1827e52e/src/gamma.jl#L65
+// Implementation based on differentiation of equation 12 of
+// K.S. Kölbig, Programs for computing the logarithm of the gamma function, and the
+// digamma function, for complex argument, Computer Physics Communications, 1972
+// https://doi.org/10.1016/0010-4655(72)90012-4.
 template <typename T>
 XSF_HOST_DEVICE inline T trigamma(T z) {
     double x = std::real(z);
-    if (x <= 0) { // reflection formula
+    if (x <= 0) {
+        // reflection formula http://dlmf.nist.gov/5.15.E6
         T s = sinpi(z);
         T c = M_PI / s;
         return c * c - trigamma(T(1) - z);
@@ -50,6 +55,7 @@ XSF_HOST_DEVICE inline T trigamma(T z) {
     int N = 10;
     int n;
     if (x < N) {
+        // recursion based on https://dlmf.nist.gov/5.15.E1
         n = N - std::floor(x);
         psi += 1.0 / (z * z);
         for (int v = 1; v < n; v++) {
@@ -58,9 +64,12 @@ XSF_HOST_DEVICE inline T trigamma(T z) {
         }
         z += n;
     }
+
+    // https://dlmf.nist.gov/5.15.E8
     T t = 1.0 / z;
     T w = t * t;
     psi += t + 0.5 * w;
+    // order of the Bernoulli numbers is reversed due to `evalpoly` convention
     double coeffs[] = {-7.092156862745098,   1.1666666666666667,   -0.2531135531135531,  0.07575757575757576,
                        -0.03333333333333333, 0.023809523809523808, -0.03333333333333333, 0.16666666666666666};
     using base_t = std::remove_cv_t<T>;
